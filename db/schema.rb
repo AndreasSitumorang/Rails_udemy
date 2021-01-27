8dc530fa1966226310e7f72d60637880c7d81914
Andreas situmorang <andreassitumorang098@gmail.com>
2021-01-27 21:42:44 +0700

    delete article

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_01_27_140345) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "news", force: :cascade do |t|
     t.string "headline"
     t.text "information"
