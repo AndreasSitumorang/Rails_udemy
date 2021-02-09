a549419f742fcb13b8a94db92f5d8c8eaccb0071
Andreas situmorang <andreassitumorang098@gmail.com>
2021-02-09 17:23:27 +0700

    Add one to many association for user and news from back end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_162607) do
+ActiveRecord::Schema.define(version: 2021_02_09_100506) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_02_08_162607) do
     t.text "information"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
