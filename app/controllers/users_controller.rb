1022b4464a0875f7fea77696da248105c689687b
Andreas situmorang <andreassitumorang098@gmail.com>
2021-02-11 16:41:54 +0700

    add edit template and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
 
   def new
-    @user =User.new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Hallo #{@user.username} your account has been updated"
+      redirect_to dailynews_index_path
+    else
+      render 'edit'
+    end
   end
 
   def create
