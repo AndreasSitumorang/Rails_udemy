a428979e90df1a1360c9d9c47175cb472b0caada
Andreas situmorang <andreassitumorang098@gmail.com>
2021-02-13 16:12:04 +0700

    add login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :dailynews
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # resources :dailynews, only: [:index, :new, :create, :show, :edit, :update] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
