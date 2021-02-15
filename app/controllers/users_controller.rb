ac82a9ece272101740bca7f98487624e5d598894
Andreas situmorang <andreassitumorang098@gmail.com>
2021-02-15 22:15:03 +0700

    add destroy for user and delete all news

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   
   def new
@@ -31,8 +31,15 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
+  def destroy
     # byebug
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Your account and new has been deleted"
+    redirect_to dailynews_index_path
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
